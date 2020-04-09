878847aec2070a229ab0f2a4c6e9ae3f2ccc7a7a
Angel Randall <drivenbysuccess21@gmail.com>
2020-04-08 21:10:12 -0500

    initial commit

---
+++ database/connection.js
@@ -0,0 +1,5 @@
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("socialnetwork", "root", "root", {host: '127.0.0.1', dialect: "mysql", operatorsAliases: false})
+
+module.exports = sequalize;
